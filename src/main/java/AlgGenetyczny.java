c972227ed6e6daa06c7677003e99586a800bbee1
Agnieszka C <dluga3@gmail.com>
2021-01-04 23:51:25 +0100

    code first steps

--- src/main/java/AlgGenetyczny.java
+++ src/main/java/AlgGenetyczny.java
@@ -11,19 +11,35 @@ public class AlgGenetyczny {
     long chromosomy[] = new long[6];
     double przystosowanie[] = new double[6];
     double przedzial[] = new double[6];
+    long rodzic[] = new long[6];
 
     private void execute() {
         init();
         obliczPrzystosowanie();
         sumaKumul();
-
+        ruletka();
 
         double suma = 0;
         for (int i = 0; i < 6; i++) {
-            System.out.println(chromosomy[i] + " " + przystosowanie[i]+" "+przedzial[i]);
+            System.out.println(chromosomy[i] + " " + przystosowanie[i] + " " + przedzial[i]);
             suma += przystosowanie[i];
         }
         System.out.println(suma);
+    }
+
+    private void ruletka() {
+
+        for (int i = 0; i < 6; i++) {
+            double traf = rnd.nextDouble();
+
+            for (int j = 0; j < 6; j++) {
+                if (traf < przedzial[j]) {
+                    rodzic[i] = chromosomy[j];
+                    System.out.println(traf+" "+j+" "+rodzic [i]);
+                    break;
+                }
+            }
+        }
     }
 
     private void sumaKumul() {
